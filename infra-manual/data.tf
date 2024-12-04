a680264e40321b3f15160d5e64ae31159acaf10f
yeswanth <yswnth.2010@gmail.com>
2024-12-04 16:06:50 +0530

    roboshop

---
+++ infra-manual/data.tf
@@ -0,0 +1,24 @@
+data "aws_route53_zone" "zone_info" {
+  name = var.domain_name
+}
+
+data "aws_ami" "joindevops" {
+
+	most_recent      = true
+	owners = ["973714476881"]
+	
+	filter {
+		name   = "name"
+		values = ["RHEL-9-DevOps-Practice"]
+	}
+	
+	filter {
+		name   = "root-device-type"
+		values = ["ebs"]
+	}
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
---
+++ infra-manual/variables.tf
@@ -0,0 +1,29 @@
+variable "instances" {
+  type        = map
+  default     = {
+    mongodb = "t3.small"
+    redis = "t3.micro"
+    mysql = "t3.small"
+    rabbitmq = "t3.micro"
+    catalogue = "t3.micro"
+    user = "t3.micro"
+    cart = "t3.micro"
+    shipping = "t3.small"
+    payment = "t3.micro"
+    dispatch = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "allow_all" {
+    type = string
+    default = "sg-0fea5e49e962e81c9"
+}
+
+# variable "zone_id" {
+#     default = "Z09912121MS725XSKH1TG"
+# }
+
+variable "domain_name" {
+    default = "sprojex.in"
+}
